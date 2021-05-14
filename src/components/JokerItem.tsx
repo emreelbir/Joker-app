e83fc3ca32a344cb120e9489d874f4f194dcd2d7
Emre Elbir <59312457+emreelbir@users.noreply.github.com>
2021-05-14 11:02:32 +0200

    Initialize project using Create React App

---
+++ src/components/JokerItem.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {CardWrapper, CardTop, CardBottom, Setup, Delivery } from "./styled/index"
+import {Joke,FlagKeys} from "../common/types"
+
+interface JokeItemProps{
+    joke:  Joke;
+}
+const JokeItem: React.FC<JokeItemProps>= ({ joke }) => {
+
+    const flags =  Object.keys(joke.flags)
+    .filter((key)=> joke.flags[key as FlagKeys])
+    .join(" , ")
+    return (
+        <CardWrapper>
+            <CardTop>
+                { joke.type === "single" ? (
+                    <p>{joke.joke}</p>
+                ):(
+                    <>
+                     <Setup>{joke.setup}</Setup>
+                     <Delivery>{joke.delivery}</Delivery>
+                     </>
+                )}
+            </CardTop>
+            <CardBottom>
+                <p>{joke.category}</p>
+                    <div>{flags}</div>
+            </CardBottom>
+        </CardWrapper>
+    )
+}
+
+export default `JokeItem`
